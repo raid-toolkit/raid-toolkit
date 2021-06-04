2c87c81782a3de229ce1ab8e9e14920f5f8877d4
Daniel Chattan <dnchattan@gmail.com>
2021-06-04 14:52:11 -0400

    update build scripts

--- just.config.js
+++ just.config.js
@@ -1,16 +1,27 @@
 // @ts-check
-const { task, series, tscTask, jestTask, parallel, eslintTask } = require('just-scripts');
+const {
+  task,
+  series,
+  tscTask,
+  jestTask,
+  parallel,
+  eslintTask,
+  prettierCheckTask,
+  prettierTask,
+} = require('just-scripts');
 const glob = require('glob');
 const path = require('path');
 
 task('ts', tscTask({ build: true }));
+
 task('test', jestTask());
-task(
-  'lint',
-  parallel(
-    ...glob
-      .sync('./packages/**/.eslintrc.js')
-      .map((config) => eslintTask({ files: [path.dirname(config)], configPath: config }))
-  )
-);
-task('ci', series('ts', parallel('test', 'lint')));
+
+task('prettier:check', prettierCheckTask({ files: ['packages/*/src/**/*.ts'] }));
+task('prettier:fix', prettierTask({ files: ['packages/*/src/**/*.ts'] }));
+
+const lintTasks = glob
+  .sync('./packages/**/.eslintrc.js')
+  .map((config) => eslintTask({ files: [path.dirname(config)], configPath: config }));
+task('lint', lintTasks.length ? parallel(...lintTasks) : () => {});
+
+task('ci', series('ts', parallel('test', 'lint', 'prettier:check')));
